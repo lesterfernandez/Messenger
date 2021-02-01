d266f40b1534edd4b7430d96ae9eb3066fb17f26
Lester Fernandez <Warlord0315@gmail.com>
2021-02-01 17:42:43 -0500

    Local Messaging server

---
+++ client.py
@@ -0,0 +1,48 @@
+import socket
+import threading
+
+HEADER = 8
+PORT = 5050
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!leave"
+SERVER = "192.168.1.74"
+ADDR = (SERVER, PORT)
+
+name = " "
+set_name = False
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
+
+
+def send(msg):
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b" " * abs((HEADER - len(send_length)))
+    client.send(send_length)
+    client.send(message)
+    
+
+
+def getInput():
+    global name
+    global set_name  
+    while True:
+        if not set_name:
+            name = input("Please enter your username: ")
+            set_name = True
+        else:
+            text = input()
+            send(f"\n{name}: {text}")
+
+
+connected = True
+thread = threading.Thread(target=getInput)
+thread.start()
+
+while connected:
+    reply_len = client.recv(HEADER).decode(FORMAT)
+    if reply_len:
+        reply_len = int(reply_len)
+        print(client.recv(reply_len).decode(FORMAT))
---
+++ server.py
@@ -0,0 +1,65 @@
+import socket 
+import threading
+
+HEADER = 8
+PORT = 5050
+SERVER = "192.168.1.74"
+ADDR = (SERVER, PORT)
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!leave"
+
+new_msg = ""
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(ADDR)
+
+chat = []
+
+conns = []
+addrs = []
+
+threadID = []
+
+def handle_client(conn, addr):
+    print(f"[NEW CONNECTION] {addr} connected.")
+
+    connected = True
+
+
+    while connected:
+        msg_length = conn.recv(HEADER).decode(FORMAT)
+        if msg_length:
+            msg_length = int(msg_length)
+            msg = conn.recv(msg_length).decode(FORMAT)
+            if msg == DISCONNECT_MESSAGE:
+                connected = False
+
+            new_msg = f"{msg}"
+            chat.append(new_msg)
+            print(new_msg)
+            for conne in conns:
+                reply = chat[-1].encode(FORMAT)
+                reply_len = len(reply)
+                reply_size = str(reply_len).encode(FORMAT)
+                reply_size += b" "*abs((HEADER - len(reply_size)))
+                conne.send(reply_size)
+                conne.send(reply)
+
+
+    conn.close()
+
+
+def start():
+    server.listen()
+    print(f"[LISTENING] Server is listening on {SERVER}")
+    while True:
+        conn, addr = server.accept()
+        conns.append(conn)
+        addrs.append(addr)
+        thread = threading.Thread(target=handle_client, args=(conn, addr))
+        thread.start()
+        print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
+
+
+print("[STARTING] server is starting...")
+start()
